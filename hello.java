7cb2bd96e4c17147a7e6a3a895959866c874f4b0
tsu <2582129519@qq.com>
2018-02-05 09:46:13 +0800

    Change Hello.java in master branch---2

--- hello.java
+++ hello.java
@@ -3,8 +3,9 @@ public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		try {
-			Emp emp = new Emp();
-			Admin admin = new Admin();
+			System.out.println("abcdefghijklmnopqrstuvwxyz");
+			System.out.println("abcdefghijklmnopqrstuvwxyz");
+			System.out.println("abcdefghijklmnopqrstuvwxyz");
 		}
 		catch () {
 
