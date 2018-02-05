6dbc50154ae4a1577e769c6c3a7b184ff2001b04
tsu <2582129519@qq.com>
2018-02-05 10:03:59 +0800

    Change Hello.java in master branch 2.0

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		Date date = new Date();
 		try {
 			System.out.println("abcdefghijklmnopqrstuvwxyz");
 			System.out.println("abcdefghijklmnopqrstuvwxyz");
