177f241555768bda3a93cf501adc3933c5ffa2fc
tsu <2582129519@qq.com>
2018-02-05 12:14:41 +0800

    0205zhongwu

--- hello.java
+++ hello.java
@@ -1,18 +1,6 @@
 import java.util.*;
 public class Hello{
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
-		Date date = new Date();
-		try {
-			Emp emp = new Emp();
-			System.out.println("DEV2");
-		}
-		catch () {
-
-		} finally {
-			System.out.println("abcdefghijklmnopqrstuvwxyz");
-			System.out.println("abcdefghijklmnopqrstuvwxyz");
-			System.out.println("abcdefghijklmnopqrstuvwxyz");
-		}	
+		System.out.println("master branch");	
 	}
 }
