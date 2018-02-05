3bb5c49360e921dbfa08458c54f21b951a4e3aa0
tsu <2582129519@qq.com>
2018-02-05 12:17:52 +0800

    Test Merge Dev

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class Hello{
 	public static void main(String[] args) {
-		System.out.println("master branch");	
+		System.out.println("dev branch");	
+		System.out.println("dev branch");
+		System.out.println("dev branch");
 	}
 }
